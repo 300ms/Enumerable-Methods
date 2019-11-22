6dd07512619934a7128fe73feb8f7c62d97c73d0
300ms <ilhansnmz44@gmail.com>
2019-11-22 19:49:41 +0300

    Code has been written and readme.md is ready now ^^

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Enumerable-Methods
+<h1 align="center">Advanced Building Blocks - Enumerable Methods</h1>
+<p align="center">
+    2nd project of "Advanced Building Blocks" part in Ruby section of Microverse Full Stack Web Development Education.
+    <br />
+</p>
+
+## Table of Contents
+
+* [About the Project](#about-the-project)
+    * [Original Description](#original-description)
+    * [Personal Description](#personal-description)
+    * [Methods List](#methods-list)
+* [Contact](#contact)
+* [Acknowledgements](#acknowledgements)
+
+## About The Project
+
+### Original Description
+
+[Full task description](https://www.theodinproject.com/courses/ruby-programming/lessons/advanced-building-blocks).
+
+### Personal Description
+
+-In this project, we are supposed to write our own methods and these methods should be and work like a few methods in 
+Enumerable Module of Ruby language. 
+
+### Methods List
+
+- my_each
+- my_each_with-index
+- my_select
+- my_all?
+- my_any?
+- my_none?
+- my_count
+- my_inject
+- multiply_els
+- my_map
+
+## Contact
+
+* Ilhan Sönmez - ilhansnmz44@gmail.com 
+
+## Acknowledgements
+
+* Microverse Full Stack Web Development Education
--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,111 +1,222 @@
-Module Enumerable
-def my_each(arr)
-  x=[]
-  i=0
-  while i<array.length
-    x.push(yield(arr[i]))
-    i+=1
-  end
-  return x
-end
+# frozen_string_literal: true
 
-def my_each_with_index(arr)
-  x=[]
-  i=0
-  while i<array.length
-    x.push(yield(arr[i],i))
-    i+=1
-  end
-  return x
-end
+module Enumerable
+  def my_each
+    return to_enum unless block_given?
 
-def my_select(arr)
-  x=[]
-  i=0
-  while i<array.length
-    if yield(arr[i])
-      x.push(arr[i])
-    end
-    i+=1
-  end
-  return x
-end
-
-def my_all?(arr)
-  x=true
-  i=0
-  while i<array.length
-    if !yield(arr[i])
-      x=false
-    end
-    i+=1
-  end
-  return x
-end
-
-def my_any?(arr)
-  i=0
-  while i<array.length
-    if yield(arr[i])
-      return true
-    end
-    i+=1
-  end
-  return false
-end
-
-def my_none?(arr)
-  x=true
-  i=0
-  while i<array.length
-    if yield(arr[i])
-      x=false
-    end
-    i+=1
-  end
-  return x
-end
-
-def my_count(arr)
-  x=0
-  i=0
-  while i<array.length
-    if yield(arr[i])
-      x+=1
-    end
-    i+=1
-  end
-  return x
-end
-
-def my_inject(arr)
-  x=arr[0]
-  i=0
-  while i<array.length
-    x=yield(x,arr[i])
-    i+=1
-  end
-  return x
-end
-
-def multiply_els(arr)
-  return my_inject(arr) do |y,n|
-    y*n
-  end
-end
-
-def my_map(arr,p=nil)
-  x=[]
-  i=0
-  while i<arr.length
-    if block_given?
-      yield(arr[i])
+    arr = self
+    if arr.is_a? Array
+      x = []
+      i = 0
+      while i < arr.length
+        x.push(yield(arr[i]))
+        i += 1
+      end
+      x
     else
-      x.push(p.call(arr[i]))
+      puts 'Error: Parameter is not an array!'
     end
-    i+=1
   end
-  return x
+
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    arr = self
+    if arr.is_a? Array
+      x = []
+      i = 0
+      while i < arr.length
+        x.push(yield(arr[i], i))
+        i += 1
+      end
+      x
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
+
+  def my_select
+    return to_enum unless block_given?
+
+    x = []
+    i = 0
+    arr = self
+    if arr.is_a? Array
+      while i < arr.length
+        x.push(arr[i]) if yield(arr[i])
+        i += 1
+      end
+      x
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
+
+  def my_all?
+    return to_enum unless block_given?
+
+    arr = self
+    if arr.is_a? Array
+      x = true
+      i = 0
+      while i < arr.length
+        x = false unless yield(arr[i])
+        i += 1
+      end
+      x
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
+
+  def my_any?
+    return to_enum unless block_given?
+
+    arr = self
+    if arr.is_a? Array
+      i = 0
+      while i < arr.length
+        return true if yield(arr[i])
+
+        i += 1
+      end
+      false
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
+
+  def my_none?
+    return to_enum unless block_given?
+
+    arr = self
+    if arr.is_a? Array
+      x = true
+      i = 0
+      while i < arr.length
+        x = false if yield(arr[i])
+        i += 1
+      end
+      x
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
+
+  def my_count
+    return to_enum unless block_given?
+
+    arr = self
+    if arr.is_a? Array
+      x = 0
+      i = 0
+      while i < arr.length
+        x += 1 if yield(arr[i])
+        i += 1
+      end
+      x
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
+
+  def my_inject
+    return to_enum unless block_given?
+
+    arr = self
+    if arr.is_a? Array
+      x = arr[0]
+      i = 1
+      while i < arr.length
+        x = yield(x, arr[i])
+        i += 1
+      end
+      x
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
+
+  def multiply_els
+    return to_enum unless block_given?
+
+    arr = self
+    if arr.is_a? Array
+      arr.my_inject do |y, n|
+        y * n
+      end
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
+
+  def my_map(prc)
+
+    arr = self
+    if arr.is_a? Array
+      x = []
+      i = 0
+      while i < arr.length
+        if block_given?
+          x.push(yield(arr[i]))
+        else
+          x.push(prc.call(arr[i]))
+        end
+        i += 1
+      end
+      x
+    else
+      puts 'Error: Parameter is not an array!'
+    end
+  end
 end
 
-p=Proc.new {|a| a**2}
+
+=begin
+---------------TEST VARIABLES------------
+
+
+arr=[1, 2, 4, 3, 5]
+
+arr2=%w[ant bear cat]
+
+arr3=(5..10).to_a
+
+prc=Proc.new {|a| a**2}
+
+
+----------------------TEST BLOCKS----------------
+
+arr.my_each do |x|
+  puts x
+end
+
+arr.my_each_with_index do |x,i|
+  if i>2
+    puts x*2
+  else
+    puts x
+  end
+end
+
+puts arr.my_select { |num| num.even?  }
+
+puts arr2.my_all? { |word| word.length >= 3 }
+
+puts arr2.my_any? { |word| word.length >= 4 }
+
+puts arr2.my_none? { |word| word.length >= 5 }
+
+puts arr.my_count { |x| x%2==0 }
+
+puts arr3.my_inject { |sum=0, n| sum + n }
+
+puts arr3.multiply_els { |sum=0, n| sum + n }
+
+puts arr3.my_map(nil) { |a| a**2 }
+
+puts arr3.my_map(prc)
+
+
+=end
+
