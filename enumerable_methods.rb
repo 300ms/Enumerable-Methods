740d7cc105f0b854c901e798e90c9a05f33bde35
300ms <ilhansnmz44@gmail.com>
2019-11-24 22:46:22 +0300

    Methods are ready for another check

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -129,6 +129,10 @@ module Enumerable
   def my_inject(initial = nil, sym = nil)
     arr = self
     arr = arr.to_a unless arr.class == Array
+    if initial.class == Symbol
+      sym = initial.to_sym
+      initial = nil
+    end
     initial.nil? ? (x = arr[0]) : (x = initial + arr[0])
     i = 1
     if sym.nil?
@@ -143,9 +147,9 @@ module Enumerable
     else
       while i < arr.length
         x = if block_given?
-              yield(x.sym(arr[i]))
+              yield(x.send(sym, arr[i]))
             else
-              [x.sym(arr[i])]
+              x.send(sym, arr[i])
             end
         i += 1
       end
@@ -196,3 +200,81 @@ end
 # rubocop: enable Layout/SpaceBeforeBlockBraces
 
 # rubocop: enable Layout/SpaceInsideBlockBraces
+
+
+puts '---------------------------------------------'
+puts 'my_each'
+array = [1, 2, 3, 4, 5].my_each { |i| i * i }
+p array
+p [1, 2, 3, 4, 5, 6].my_each #=> Enumerator
+
+puts '---------------------------------------------'
+puts 'my_each_with_index'
+hash = {}
+%w[cat dog wombat].my_each_with_index do |item, index|
+  hash[item] = index
+end
+puts hash #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+p [1, 2, 3, 4, 5, 6].my_each_with_index #=> Enumerator
+
+puts '---------------------------------------------'
+puts 'my_select'
+p [1, 2, 3, 4, 5].my_select { |num| num.even? } #=> [2, 4]
+arr = [12.2, 13.4, 15.5, 16.9, 10.2]
+new_arr = arr.my_select do |num|
+  num.to_f > 13.3
+end
+p new_arr #=> [13.4, 15.5, 16.9]
+p [12.2, 13.4, 15.5, 16.9, 10.2].my_select #=> Enumerator
+
+puts '---------------------------------------------'
+puts 'my_all?'
+puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+puts %w[ant bear cat].my_all?(/t/) #=> false
+puts [1, 2i, 3.14].my_all?(Numeric) #=> true
+puts [nil, true, 99].my_all? #=> false
+puts [].my_all? #=> true
+
+puts '---------------------------------------------'
+puts 'my_any?'
+puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+puts %w[ant bear cat].my_any?(/d/) #=> false
+puts [nil, true, 99].my_any?(Integer) #=> true
+puts [nil, true, 99].my_any? #=> true
+puts [].my_any? #=> false
+
+puts '---------------------------------------------'
+puts 'my_none?'
+puts %w[ant bear cat].my_none? { |word| word.length == 5 } #=> true
+puts %w[ant bear cat].my_none? { |word| word.length >= 4 } #=> false
+puts %w[ant bear cat].my_none?(/d/) #=> true
+puts [1, 3.14, 42].my_none?(Float) #=> false
+puts [].my_none? #=> true
+puts [nil].my_none? #=> true
+puts [nil, false].my_none? #=> true
+puts [nil, false, true].my_none? #=> false
+
+puts '---------------------------------------------'
+puts 'my_count'
+ary = [1, 2, 4, 2]
+puts ary.my_count #=> 4
+puts ary.my_count(2) #=> 2
+puts ary.my_count { |x| (x % 2).zero? } #=> 3
+
+puts '---------------------------------------------'
+puts 'my_map'
+p [1, 2, 3, 4].my_map { |i| i * i } #=> [1, 4, 9, 16]
+p [1, 2, 3, 4].my_map #=> Enumerator
+
+puts '---------------------------------------------'
+puts 'my_inject'
+puts (5..10).my_inject { |sum, n| sum + n } #=> 45
+puts (5..10).my_inject :+ #=> 45
+puts (5..10).inject(1) { |product, n| product * n } #=> 151200
+puts (5..10).my_inject(1, :*) #=> 151200
+longest = %w[cat sheep bear].inject do |memo, word|
+  memo.length > word.length ? memo : word
+end
+puts longest #=> 'sheep'
