1adb7d654bf664e4467cc62dc163dc47e4628f5b
300ms <ilhansnmz44@gmail.com>
2019-11-24 17:01:59 +0300

    Code passes from tests on original page

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -58,98 +58,143 @@ module Enumerable
     end
   end
 
-  def my_all?
+  def my_all?(cls = nil)
     arr = self
-    if arr.is_a? Array
+    if cls.nil?
+      if arr.is_a? Array
+        x = true
+        i = 0
+        while i < arr.length
+          if block_given?
+            x = false unless yield(arr[i])
+          else
+            x = false unless arr[i]
+          end
+          i += 1
+        end
+        x
+      else
+        puts 'Error: Parameter is not an array!'
+      end
+    else
       x = true
       i = 0
+      cls.class == Integer.class ? cls = cls : cls = cls.class
+      puts cls
       while i < arr.length
-        if block_given?
-          x = false unless yield(arr[i])
-        else
-          x = false unless arr[i]
-        end
+        x = false unless arr[i].class == cls
         i += 1
       end
       x
-    else
-      puts 'Error: Parameter is not an array!'
     end
   end
 
-  def my_any?
+  def my_any?(cls = nil)
     arr = self
-    if arr.is_a? Array
-      i = 0
-      while i < arr.length
-        if block_given?
-          return true if yield(arr[i])
-        else
-          return true if arr[i]
+    if cls.nil?
+      if arr.is_a? Array
+        i = 0
+        while i < arr.length
+          if block_given?
+            return true if yield(arr[i])
+          else
+            return true if arr[i]
+          end
+          i += 1
         end
+        false
+      else
+        puts 'Error: Parameter is not an array!'
+      end
+    else
+      i = 0
+      cls.class == Integer.class ? cls = cls : cls = cls.class
+      while i < arr.length
+        return true if arr[i].class == cls
         i += 1
       end
       false
-    else
-      puts 'Error: Parameter is not an array!'
     end
   end
 
-  def my_none?
+  def my_none?(cls = nil)
     arr = self
-    if arr.is_a? Array
+    if cls.nil?
+      if arr.is_a? Array
+        x = true
+        i = 0
+        while i < arr.length
+          if block_given?
+            x = false if yield(arr[i])
+          else
+            x = false if arr[i]
+          end
+          i += 1
+        end
+        x
+      else
+        puts 'Error: Parameter is not an array!'
+      end
+    else
       x = true
       i = 0
+      cls.class == Integer.class ? cls = cls : cls = cls.class
+      puts cls
       while i < arr.length
-        if block_given?
-          x = false if yield(arr[i])
-        else
-          x = false if arr[i]
-        end
+        x = false if arr[i].class == cls
         i += 1
       end
       x
-    else
-      puts 'Error: Parameter is not an array!'
     end
   end
 
-  def my_count
+  def my_count(item = nil)
     arr = self
-    if arr.is_a? Array
-      x = 0
-      i = 0
-      while i < arr.length
-        if block_given?
-          x += 1 if yield(arr[i])
-        else
-          x += 1 if arr[i]
+    if item.nil?
+      if arr.is_a? Array
+        x = 0
+        i = 0
+        while i < arr.length
+          if block_given?
+            x += 1 if yield(arr[i])
+          else
+            x += 1 if arr[i]
+          end
+          i += 1
         end
-        i += 1
+        x
+      else
+        puts 'Error: Parameter is not an array!'
       end
-      x
     else
-      puts 'Error: Parameter is not an array!'
+      if arr.is_a? Array
+        x = 0
+        i = 0
+        while i < arr.length
+          x += 1 if item == arr[i]
+          i += 1
+        end
+        x
+      else
+        puts 'Error: Parameter is not an array!'
+      end
     end
   end
 
-  def my_inject
+  def my_inject(initial = nil, sym = nil)
     arr = self
-    if arr.is_a? Array
-      x = arr[0]
-      i = 1
-      while i < arr.length
-        x = if block_given?
-              yield(x, arr[i])
-            else
-              [x, arr[i]]
-            end
-        i += 1
-      end
-      x
-    else
-      puts 'Error: Parameter is not an array!'
+    arr = arr.to_a unless arr.class == Array
+    initial.nil? ? (x = arr[0]) : (x = initial + arr[0])
+    i = 1
+    while i < arr.length
+      x = if block_given?
+            yield(x, arr[i])
+          else
+            [x, arr[i]]
+          end
+      i += 1
     end
+    x
   end
 
   def multiply_els
@@ -163,23 +208,20 @@ module Enumerable
     end
   end
 
-  def my_map(prc)
+  def my_map(prc = nil)
     arr = self
-    if arr.is_a? Array
-      x = []
-      i = 0
-      while i < arr.length
-        if block_given?
-          x.push(yield(arr[i]))
-        else
-          x.push(prc.call(arr[i]))
-        end
-        i += 1
+    arr = arr.to_a if arr.class != Array
+    x = []
+    i = 0
+    while i < arr.length
+      if block_given?
+        x.push(yield(arr[i]))
+      else
+        x.push(prc.call(arr[i]))
       end
-      x
-    else
-      puts 'Error: Parameter is not an array!'
+      i += 1
     end
+    x
   end
 end
 
